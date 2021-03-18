27c31ad9ef8a128d1462c657914e4bb64c957fe2
Paul Ly <paul.ly@live.ca>
2021-03-18 18:25:50 -0400

    help

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
 import ContactKeeper from './components/pages/ContactKeeper'
 import GithubSearcher from './components/pages/GithubSearcher'
