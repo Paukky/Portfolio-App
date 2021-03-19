29cb6e8cd04469c789f4764cccef5136ef37fb19
Paul Ly <paul.ly@live.ca>
2021-03-18 21:01:01 -0400

    I HOPE IT WORKS THIS TIME

--- src/components/pages/ContactKeeper.js
+++ src/components/pages/ContactKeeper.js
@@ -2,14 +2,33 @@ import React from 'react'
 import ContactKeepers from '../layout/ContactKeeper.png'
 import ContactKeepers2 from '../layout/ContactKeeper2.png'
 import ContactKeepers3 from '../layout/ContactKeeper3.png'
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const ContactKeeper = () =>  {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
 
+    };
         return (      
             <React.Fragment>
                 <div className="container p-2 ">
-                    
+                    <Slider {...settings}>
+                        <div>
+                            <img src={ContactKeepers} width= "100%"/>
+                        </div>
+                        <div>
+                            <img src={ContactKeepers2} width= "100%"/>
+                        </div>
+                        <div>
+                            <img src={ContactKeepers3} width= "100%"/>
+                        </div>
+                    </Slider>
                     <p className ="text-center text-primary p-2">
                     This is my first full stack application created for learning the back-end. 
                     I used postman for creating my API and use MongoDB to serve as my Cloud Database. You can view this project here
--- src/components/pages/GithubSearcher.js
+++ src/components/pages/GithubSearcher.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import GithubSearchers from '../layout/GithubSearcher.png'
 import GithubSearchers2 from '../layout/GithubSearcher2.png'
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const ContactKeeper = () =>  {
-
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: true,
+        }
         return (      
             <React.Fragment>
                 <div className="container p-2 ">
-                    <Carousel
-                        plugins={[
-                            'arrows',
-                            'centered',
-                            'fastSwipe',
-                            'infinite'
-                        ]}
-                    >
-                    <img src={GithubSearchers} width="100%"/>
-                    <img src={GithubSearchers2} width="100%"/>         
-                    </Carousel>
+                    <Slider>
+                        <div>
+                             <img src={GithubSearchers} width="100%"/>
+                        </div>
+                        
+                        <div>
+                             <img src={GithubSearchers2} width="100%"/>
+                        </div>     
+
+                    </Slider> 
+                    
                     <p className ="text-center text-primary p-2">
                     Using Github's REST API, I was able to integrate it into this application and being able to call for data. 
                     This is my first front-end application that taught me the basic of React. It's scalable and easy to use. 
